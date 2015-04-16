27009d037f7494cc7a068ea7fb39d53fdb4e1851
Ness Morris <karplusstrong@gmail.com>
2015-04-15 23:55:11 -0700

    made simple spiral into pattern variation subclass

---
+++ Assets/Scripts/SimpleSpiral.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class SimpleSpiral : PatternVariation{
+	
+	private int nBullets = 8;
+	private float mag = 1.0f;
+	private float offset = 0.075f;
+	private float angleWidth = 1.0f;
+	private const float TAU = Mathf.PI * 2f;
+	
+	public override IEnumerator Fire(){
+		if(reversed){
+			offset += 0.5f;
+		}
+		while(true){
+			for(int i=0;i<nBullets;i++){
+				float phase = (float)i/nBullets;
+				float theta = (phase * TAU * angleWidth) + TAU*offset;
+				Vector3 bulletPosition = new Vector3(
+						Mathf.Sin(theta)*mag, Mathf.Cos(theta)*mag, 0);
+
+				Bullet thisBullet = GameObject.Instantiate(
+						bullet,
+						(bulletPosition + transform.position),
+						Quaternion.identity) as Bullet;
+
+				thisBullet.velocity = bulletPosition.normalized * bulletSpeed;
+			}
+			offset += 0.01625f;
+			yield return new WaitForSeconds(0.2f);
+		}
+	}
+}
