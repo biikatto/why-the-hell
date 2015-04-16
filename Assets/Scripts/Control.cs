15de08882514500aef7fd92347f6f709f7b677c9
Ness Morris <ness@kadenze.com>
2015-04-16 16:45:49 -0700

    let's try again

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Control : MonoBehaviour{
+	[SerializeField]
+	private bool player2;
+	public bool Player2{
+		get{
+			return player2;
+		}
+		set{
+			player2 = value;
+		}
+	}
+
+	private Movement movement;
+	private Weapon weapon;
+
+    private Hashtable inputList;
+
+    public void Start(){
+    	movement = gameObject.GetComponent<Ship>().Movement;
+    	weapon = gameObject.GetComponent<Ship>().Weapon;
+
+    	inputList = new Hashtable();
+    	inputList.Add("Horizontal", "Horizontal");
+    	inputList.Add("Vertical", "Vertical");
+    	inputList.Add("Fire", "Fire");
+    	foreach(string key in ((Hashtable)inputList.Clone()).Keys){
+    		if(player2){
+    			inputList[key] = inputList[key] + " P2";
+    		}else{
+    			inputList[key] = inputList[key] + " P1";
+    		}
+    	}
+    }
+
+	public void LateUpdate(){
+		Vector2 movementVector = new Vector2();
+		if(Input.GetAxis(inputList["Horizontal"] as string) > 0){
+			// move right
+			movementVector.x = 1;
+		}else if (Input.GetAxis(inputList["Horizontal"] as string) < 0){
+			// move left
+			movementVector.x = -1;
+		}
+
+		if(Input.GetAxis(inputList["Vertical"] as string) > 0){
+			// move up
+			movementVector.y = 1;
+		}else if(Input.GetAxis(inputList["Vertical"] as string) < 0){
+			// move down
+			movementVector.y = -1;
+		}
+		movement.Move(movementVector.normalized);
+
+		if(Input.GetButtonDown(inputList["Fire"] as string)){
+			// begin fire
+			weapon.BeginFire();
+			movement.Firing(true);
+		}
+		if(Input.GetButtonUp(inputList["Fire"] as string)){
+			// end fire
+			weapon.EndFire();
+			movement.Firing(false);
+		}
+	}
+}
---
+++ Assets/Scripts/Patterns/SinPattern.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class SinPattern : BulletPattern{
+	
+	private int nBullets = 8;
+	private float mag = 1.0f;
+	private float offset = 0.075f;
+	private float sinWidth = 0.25f;
+	private float sinFreq = 2f;
+	private const float TAU = Mathf.PI * 2f;
+	
+	public override IEnumerator Fire(){
+		Debug.Log("firing");
+		if(reversed){
+			offset += 0.5f;
+		}
+		while(true){
+			for(int i=0;i<nBullets;i++){
+				float theta = ((float)i/nBullets) * TAU;
+				Vector3 bulletPosition = new Vector3(
+						Mathf.Sin(theta)*mag, Mathf.Cos(theta)*mag, 0);
+				Bullet thisBullet = GameObject.Instantiate(
+						bullet,
+						(bulletPosition + transform.position),
+						Quaternion.identity) as Bullet;
+				thisBullet.velocity = bulletPosition.normalized * bulletSpeed;
+				thisBullet.sinPattern = true;
+				thisBullet.sinWidth = sinWidth;
+				thisBullet.sinFreq = sinFreq;
+			}
+			offset += 0.01625f;
+			yield return new WaitForSeconds(0.2f);
+		}
+	}
+}
