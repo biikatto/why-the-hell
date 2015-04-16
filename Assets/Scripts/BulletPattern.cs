035e40f142b42b8ac2ad8ccda6153af84b346cba
Ness Morris <karplusstrong@gmail.com>
2015-04-15 22:32:06 -0700

    added sin wave bullet pattern

--- Assets/Scripts/BulletPattern.cs
+++ Assets/Scripts/BulletPattern.cs
@@ -19,6 +19,8 @@ public class BulletPattern : MonoBehaviour{
 
 	private float cooldownTime = 0f;
 	private float cooldownDuration = 0.4f;
+
+	private float tau;
 	
 	private int patternNumber;
 	public int PatternNumber{
@@ -27,9 +29,11 @@ public class BulletPattern : MonoBehaviour{
 	}
 
 	public void Start(){
+		tau = 2*Mathf.PI;
 		firing = false;
 		bulletSpeed = 6.3f;
 		readyToFire = true;
+		patternNumber = 0;
 	}
 
 	public void BeginFire(bool reversed){
@@ -60,41 +64,90 @@ public class BulletPattern : MonoBehaviour{
 	}
 
 	private IEnumerator Fire(bool reversed){
-		float tau = 2*Mathf.PI;
 		float delay = 0.2f;
+		Coroutine patternCoroutine = null;
 		while(!readyToFire){
 			yield return new WaitForFixedUpdate();
 		}
+		switch(patternNumber){
+			case 0:
+				Debug.Log("sin");
+				patternCoroutine = StartCoroutine(SinPattern(reversed));
+				break;
+
+			default:
+				Debug.Log("spiral");
+				patternCoroutine = StartCoroutine(BasicPattern(reversed));
+				break;
+		}
+		yield return new WaitForSeconds(delay);
 		while(firing){
-			switch(patternNumber){
-				default:
-					int nBullets = 8;
-					float angleWidth = 1.0f;
-					float mag = 1.0f;
-					float offset = 0.075f;
-					if(reversed){
-						offset += 0.5f;
-					}
-					while(firing){
-					    for(int i=0;i<nBullets;i++){
-						    float theta = (((float)i/nBullets) *
-											    tau*angleWidth) +
-												    tau*offset;
-						    Vector3 bulletPosition = new Vector3(
-								    Mathf.Sin(theta)*mag, Mathf.Cos(theta)*mag, 0);
-						    Bullet thisBullet = GameObject.Instantiate(
-							    bullet,
-							    (new Vector3(Mathf.Sin(theta)*mag, Mathf.Cos(theta)*mag, 0) +
-								    transform.position),
-							    Quaternion.identity) as Bullet;
-						    thisBullet.velocity = bulletPosition.normalized * bulletSpeed;
-					    }
-					    offset += 0.01625f;
-						yield return new WaitForSeconds(0.2f);
-					}
-					break;
+			yield return new WaitForFixedUpdate();
+		}
+		StopCoroutine(patternCoroutine);
+	}
+
+	private IEnumerator BasicPattern(bool reversed){
+		int nBullets = 8;
+		float angleWidth = 1.0f;
+		float mag = 1.0f;
+		float offset = 0.075f;
+		if(reversed){
+			offset += 0.5f;
+		}
+		while(firing){
+			for(int i=0;i<nBullets;i++){
+				float theta = (((float)i/nBullets) *
+						tau*angleWidth) +
+					tau*offset;
+				Vector3 bulletPosition = new Vector3(
+						Mathf.Sin(theta)*mag, Mathf.Cos(theta)*mag, 0);
+
+				Bullet thisBullet = GameObject.Instantiate(
+						bullet,
+						(new Vector3(Mathf.Sin(theta)*mag, Mathf.Cos(theta)*mag, 0) +
+						 transform.position),
+						Quaternion.identity) as Bullet;
+				thisBullet.velocity = bulletPosition.normalized * bulletSpeed;
 			}
-			yield return new WaitForSeconds(delay);
+			offset += 0.01625f;
+			yield return new WaitForSeconds(0.2f);
+		}
+	}
+
+	private IEnumerator SinPattern(bool reversed){
+		int nBullets = 8;
+		float angleWidth = 1.0f;
+		float mag = 1.0f;
+		float offset = 0.075f;
+		float sinWidth = 0.25f;
+		float sinFreq = 2f;
+		float rotationSpeed = 1f;
+		if(reversed){
+			offset += 0.5f;
+		}
+		while(firing){
+			for(int i=0;i<nBullets;i++){
+				// float theta = (((float)i/nBullets) *
+				// 		tau*angleWidth) +
+				// 		(Mathf.Sin(tau*offset) * sinWidth);
+				// float theta = (((float)i/nBullets) *
+				// 		tau*angleWidth) +
+				// 		tau*offset;
+				float theta = ((float)i/nBullets) * tau;
+				Vector3 bulletPosition = new Vector3(
+						Mathf.Sin(theta)*mag, Mathf.Cos(theta)*mag, 0);
+				Bullet thisBullet = GameObject.Instantiate(
+						bullet,
+						(bulletPosition + transform.position),
+						Quaternion.identity) as Bullet;
+				thisBullet.velocity = bulletPosition.normalized * bulletSpeed;
+				thisBullet.sinPattern = true;
+				thisBullet.sinWidth = sinWidth;
+				thisBullet.sinFreq = sinFreq;
+			}
+			offset += 0.0325f * rotationSpeed;
+			yield return new WaitForSeconds(0.2f);
 		}
 	}
 }
