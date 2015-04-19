822db7187442b4d6be6fa96bab70e064921432c6
Ness Morris <karplusstrong@gmail.com>
2015-04-19 16:49:06 -0700

    actually fix merge

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -19,30 +19,16 @@ public class Control : MonoBehaviour{
 
     private Hashtable inputList;
 
-    private bool reversed;
-
     public void Start(){
     	movement = gameObject.GetComponent<Ship>().Movement;
     	weapon = gameObject.GetComponent<Ship>().Weapon;
 
-    	if(player2){
-    	    reversed = true;
-    	}else{
-    	    reversed = false;
-    	}
-
     	inputList = new Hashtable();
     	inputList.Add("Horizontal", "Horizontal");
     	inputList.Add("Vertical", "Vertical");
-<<<<<<< HEAD
     	for(int i=0;i<4;i++){
     		inputList.Add("Fire "+(i+1), "Fire "+(i+1));
     	}
-=======
-    	inputList.Add("Fire", "Fire");
-    	inputList.Add("Increment Pattern", "Increment Pattern");
-    	inputList.Add("Decrement Pattern", "Decrement Pattern");
->>>>>>> df8bd13949797a7e27681b98b90c1e0a15d41dd5
     	foreach(string key in ((Hashtable)inputList.Clone()).Keys){
     		if(player2){
     			inputList[key] = inputList[key] + " P2";
@@ -71,11 +57,10 @@ public class Control : MonoBehaviour{
 		}
 		movement.Move(movementVector.normalized);
 
-<<<<<<< HEAD
 		for(int i=0;i<4;i++){
 			if(Input.GetButtonDown(inputList["Fire "+(i+1)] as string)){
 				// begin fire
-				weapon.BeginFire(i, player2);
+				weapon.BeginFire(i);
 				movement.Firing(weapon.Firing);
 			}
 			if(Input.GetButtonUp(inputList["Fire "+(i+1)] as string)){
@@ -83,26 +68,6 @@ public class Control : MonoBehaviour{
 				weapon.EndFire();
 				movement.Firing(weapon.Firing);
 			}
-=======
-		if(Input.GetButtonDown(inputList["Fire"] as string)){
-			// begin fire
-			weapon.BeginFire(reversed);
-			movement.Firing(true);
-		}
-		if(Input.GetButtonUp(inputList["Fire"] as string)){
-			// end fire
-			weapon.EndFire();
-			movement.Firing(false);
->>>>>>> df8bd13949797a7e27681b98b90c1e0a15d41dd5
-		}
-
-		if(Input.GetButtonDown(inputList["Increment Pattern"] as string)){
-		    // increment pattern
-		    weapon.PatternNumber = weapon.PatternNumber + 1;
-		}
-		if(Input.GetButtonDown(inputList["Decrement Pattern"] as string)){
-		    // decrement Pattern
-		    weapon.PatternNumber = weapon.PatternNumber - 1;
 		}
 	}
 }
